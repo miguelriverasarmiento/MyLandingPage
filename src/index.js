3c80b754bbbe3150c3225892cb73e2f273600a38
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2021-02-15 19:27:39 -0500

    add database

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
 
 // Settings
 app.set('port', 4000);
@@ -9,6 +10,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
+// connecting to db
+mongoose.connect('mongodb://localhost:27017')
+  .then(db => console.log('Db connected'))
+  .catch(err => console.log(err));
 // Middlewares
 /*app.use(express.json());
 app.use(morgan('dev'));*/
