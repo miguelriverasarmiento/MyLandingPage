e49eb30a124a257152d65f7b374da774130dd49c
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2021-03-01 16:41:24 -0500

    add DB modified

--- src/controller/emailsController.js
+++ src/controller/emailsController.js
@@ -1,11 +1,13 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
+
     user: 'postgres',
     host: 'localhost',
     database: 'landingPage',
     password: 'mr',
     port: 5432,
 })
+
 const createUser = (req, res) => {
     const {email} = req.body
 
@@ -20,36 +22,3 @@ const createUser = (req, res) => {
 module.exports = {
     createUser,
 }
-/*var mongoose = require('mongoose');
-var User = require('../model/user');
-
-function saveEmail(req, res){
-
-    var user = new User();
-    var params = req.body;
-
-    if(params.email){
-           
-        user.email = params.email;
-        user.save((err, emailStored) => {
-
-            if(err) return res.status(500).send({message: 'Server mistake'});
-            if(emailStored){
-                return res.status(200).send({
-                    user: emailStored
-                });
-            }else{
-                return res.status(200).send({
-                    message: 'Not saved'
-                });
-            }
-        });
-
-    }else{
-        return res.status(200).send({
-            message: 'Email is required'
-        });
-    }
-}
-
-module.exports = { saveEmail };*/
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
-const methodOverride = require("method-override");
-//var User = require('./model/user');
+
 // Settings
 app.set('port', 4000);
 app.set('views', path.join(__dirname, 'views'));
@@ -12,10 +11,6 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(methodOverride());
-
-// connecting to db
-
 
 // Routes
 app.use(require('./routes/index'));
@@ -25,7 +20,3 @@ app.use(express.static('src'));
 app.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
 });
-
-
-
-
